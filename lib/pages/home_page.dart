22a040d17dc1aa3a3317b0afc7fbf3760bdb6013
unknown <shafinachaerunisa@gmail.com>
2024-06-13 19:51:20 +0700

    add sidebar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,47 +8,104 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
-  late TabController _tabController;
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 2, vsync: this);
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    Container(
+      color: Colors.grey[900],
+      child: Center(
+        child: Text(
+          'Home Page Content',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Restaurant X'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.login),
-            onPressed: () {
-              // Handle login action
-            },
-          ),
-        ],
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: const [
-            Tab(text: 'Menu'),
-            Tab(text: 'Reservation'),
-          ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.blueGrey,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          color: Colors.blueGrey,
         ),
       ),
-      body: TabBarView(
-        controller: _tabController,
-        children: const [
-          // UploadImageAndMore(),
-          ReservationPage(),
-        ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Restaurant X'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.account_circle_rounded),
+              onPressed: () {
+                // Handle login action
+              },
+            ),
+          ],
+        ),
+        body: _pages[_selectedIndex],
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                ),
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Restaurant X',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('Home'),
+                onTap: () {
+                  _onItemTapped(0);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.menu_book),
+                title: const Text('Menu'),
+                onTap: () {
+                  _onItemTapped(1);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.book),
+                title: const Text('Reservation'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ReservationPage()),
+                  ).then((_) {
+                    // Ensure the drawer closes when coming back from ReservationPage
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
