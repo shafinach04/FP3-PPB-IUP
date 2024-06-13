c17629b0c4d25bfeb0682ffe3319d1bcc788d949
unknown <shafinachaerunisa@gmail.com>
2024-06-13 20:02:33 +0700

    enhance ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:ppb_fp/pages/reservation_page.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.blueGrey,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          color: Colors.blueGrey,
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -13,7 +34,7 @@ class _HomePageState extends State<HomePage> {
 
   final List<Widget> _pages = [
     Container(
-      color: Colors.grey[900],
+      color: Colors.grey[850],
       child: Center(
         child: Text(
           'Home Page Content',
@@ -21,6 +42,7 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     ),
+    //add pic slide of menu
   ];
 
   void _onItemTapped(int index) {
@@ -29,83 +51,81 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void _navigateToHome(BuildContext context) {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => HomePage()),
+          (route) => false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Colors.blueGrey,
-        scaffoldBackgroundColor: Colors.black,
-        appBarTheme: AppBarTheme(
-          color: Colors.blueGrey,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Restaurant X'),
+        backgroundColor: Colors.blueGrey,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.account_circle_rounded),
+            onPressed: () {
+              // Handle login action
+            },
+          ),
+        ],
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Restaurant X'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.account_circle_rounded),
-              onPressed: () {
-                // Handle login action
-              },
-            ),
-          ],
-        ),
-        body: _pages[_selectedIndex],
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                ),
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Restaurant X',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
+      body: _pages[_selectedIndex],
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+              ),
+              child: Container(
+                alignment: Alignment.center,
+                child: Text(
+                  'Restaurant X',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
                   ),
                 ),
               ),
-              ListTile(
-                leading: const Icon(Icons.home),
-                title: const Text('Home'),
-                onTap: () {
-                  _onItemTapped(0);
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.menu_book),
-                title: const Text('Menu'),
-                onTap: () {
-                  _onItemTapped(1);
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.book),
-                title: const Text('Reservation'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ReservationPage()),
-                  ).then((_) {
-                    // Ensure the drawer closes when coming back from ReservationPage
-                    Navigator.pop(context);
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                _navigateToHome(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.menu_book),
+              title: const Text('Menu'),
+              onTap: () {
+                _onItemTapped(1);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.book),
+              title: const Text('Reservation'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ReservationPage()),
+                ).then((_) {
+                  setState(() {
+                    _selectedIndex = 0; // Ensure HomePage is selected
                   });
-                },
-              ),
-            ],
-          ),
+                });
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
